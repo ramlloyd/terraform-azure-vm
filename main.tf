233c1895b81f19e4a942b817f6be48306eecefb5
ramlloyd <cherukuriramu5@gmail.com>
2025-05-21 19:44:22 +0530

    Fresh commit - .terraform folder excluded

---
+++ main.tf
@@ -0,0 +1,73 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "baca3462-6e15-428a-9161-cb78d5a78fc2"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-tf"
+  location = "eastus"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "vm-tf"
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  size                            = "Standard_B1s"
+  admin_username                  = "azure"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azure"
+    public_key = file("C:/Users/Dell/.ssh/id_rsa.pub")
+  }
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-tf"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-tf"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.0.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "public_ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "NIC"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
